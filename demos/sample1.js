8a975b023a256cf20236cdbef238a5634323b133
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2020-11-11 02:20:36 +0700

    fix cannot redirect

--- demos/sample1.js
+++ demos/sample1.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send(req.headers);
 });
 
+app.get('/redirect', (req, res) => {
+  res.redirect('/');
+});
+
 app.listen(3000, () => console.log('Server is listening on port 3000...'));
