8c59b25f9ab1a206010ef7fdc7e37b535bd017ec
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2020-11-18 02:30:08 +0700

    update test

--- test/router.js
+++ test/router.js
@@ -2,20 +2,18 @@ const { Router } = require('..');
 
 const router = Router();
 
-router.get('/hello/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}`);
-});
-
-router.get('/query', (req, res) => {
-  res.send(`Query: ${req.query.s}`);
-});
-
-router.post('/search/:s?', (req, res) => {
-  res.send({ hasParams: !!req.params.s });
-});
-
-router.post('/post-body', (req, res) => {
-  res.send(req.body);
-});
+router
+  .get('/hello/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+  })
+  .get('/query', (req, res) => {
+    res.send(`Query: ${req.query.s}`);
+  })
+  .post('/search/:s?', (req, res) => {
+    res.send({ hasParams: !!req.params.s });
+  })
+  .post('/post-body', (req, res) => {
+    res.send(req.body);
+  });
 
 module.exports = router;
